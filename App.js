a8737bc8aefe76da8997b942f773a72af1c0d159
Fabriciooo <fabricerob@yahoo.fr>
2024-01-29 17:48:59 +0100

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import HomeScreen from "./src/screens/HomeScreen";
+import RestaurantDetailsPage from "./src/screens/RestaurantDetailsScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <HomeScreen /> */}
+      <RestaurantDetailsPage />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  restaurantContainer: {
+    width: "100%",
+    marginVertical: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 4 / 3,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "500",
+    marginVertical: 5,
+  },
+  subtitle: {
+    color: "grey",
   },
 });
